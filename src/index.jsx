fc71dfd8dcf5870ca4929babb52aa4f0260d6ec9
leslie.alldridge <leslie.alldridge@gmail.com>
2020-04-26 08:03:21 +1200

    committing code with eslint error

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
