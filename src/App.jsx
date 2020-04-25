388ede5052e4aed713010caffb9ba5fbbb42e8eb
Leslie Alldridge <leslie.alldridge@gmail.com>
2020-04-26 11:25:51 +1200

    Adding styles, linting and a new directory layout (#1)
    Adding styles, linting and a new directory layout and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Footer from './common/Footer/Footer';
 
 function App() {
   return (
@@ -8,17 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code> src/App.js </code>
+          and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Footer name="Leslie Alldridge" />
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders default CRA text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/and save to reload/i);
   expect(linkElement).toBeInTheDocument();
 });
